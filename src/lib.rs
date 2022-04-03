04798a055eaf688ba3f08d80756069e1c35491cc
unknown <krasedgar23@gmail.com>
2022-04-03 20:10:07 +0200

    matrix struct and construction test

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
+mod matrix;
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn it_tests_matrix_construction() {
+        use crate::matrix::matrix;
+        let shape = matrix::Matrix::new(2, 2).shape();
+        let data = matrix::Matrix::new(2, 2).as_vec();
+        let vec = vec![vec![0.0; 2]; 2];
+        assert_eq!((vec, (2, 2)), (data, shape));
     }
 }
---
+++ src/matrix.rs
@@ -0,0 +1,22 @@
+#[allow(unused)]
+
+pub mod matrix {
+    pub struct Matrix {
+        shape: (i32, i32),
+        data: Vec<Vec<f64>>
+    }
+    impl Matrix {
+        pub fn new(rows: i32, cols: i32) -> Matrix {
+            Matrix {
+                shape: (rows, cols),
+                data: vec![vec![0.0; cols as usize]; rows as usize]
+            }
+        }
+        pub fn shape(&self) -> (i32, i32) {
+            self.shape
+        }
+        pub fn as_vec(&self) -> Vec<Vec<f64>> {
+            self.data.clone()
+        }
+    }
+}
