9974d1e5d76217e4b4db9ab9eaf28c25de44fd6b
unknown <krasedgar23@gmail.com>
2022-04-03 20:16:40 +0200

    vector struct and construction test

---
+++ src/vector.rs
@@ -0,0 +1,20 @@
+pub mod vector {
+    pub struct Vector {
+        shape: i32,
+        data: Vec<f64>
+    }
+    impl Vector {
+        pub fn new(shape: i32) -> Vector {
+            Vector {
+                shape,
+                data: vec![0.0; shape as usize]
+            }
+        }
+        pub fn shape(&self) -> i32 {
+            self.shape
+        }
+        pub fn as_vec(&self) -> Vec<f64> {
+            self.data.clone()
+        }
+    }
+}
