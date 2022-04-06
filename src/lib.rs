11ddd17dda7dc8a5e2034124da6dacad1142088e
unknown <krasedgar23@gmail.com>
2022-04-06 16:44:58 +0200

    Removed janky module implementations

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-mod matrix;
-mod vector;
+pub mod matrix;
+pub mod vector;
 #[cfg(test)]
 mod tests {
     use std::vec;
 
     #[test]
     fn it_tests_vector_construction() {
-        use crate::vector::vector;
+        use crate::vector;
         let shape = vector::Vector::new(2).shape();
         let data = vector::Vector::new(2).as_vec();
         let vec = vec![0.0; 2];
@@ -14,14 +14,14 @@ mod tests {
     }
     #[test]
     fn it_tests_vector_to_scalar_addition() {
-        use crate::vector::vector;
+        use crate::vector;
         let mut v = vector::Vector::new(2);
         v += 1;
         assert_eq!(v.as_vec(), vec![1.0, 1.0]);
     }
     #[test]
     fn it_tests_vector_to_scalar_subtraction() {
-        use crate::vector::vector;
+        use crate::vector;
         let mut v = vector::Vector::new(2);
         v += 2;
         v -= 1;
@@ -29,7 +29,7 @@ mod tests {
     }
     #[test]
     fn it_tests_vector_to_scalar_multiplication() {
-        use crate::vector::vector;
+        use crate::vector;
         let mut v = vector::Vector::new(2);
         v += 1;
         v *= 2;
@@ -37,7 +37,7 @@ mod tests {
     }
     #[test]
     fn it_tests_vector_to_scalar_division() {
-        use crate::vector::vector;
+        use crate::vector;
         let mut v = vector::Vector::new(2);
         v += 2;
         v /= 2;
@@ -45,7 +45,7 @@ mod tests {
     }
     #[test]
     fn it_tests_vector_to_vector_addition() {
-        use crate::vector::vector;
+        use crate::vector;
         let mut v1 = vector::Vector::new(2);
         v1.set(vec![2.0, 3.0]);
         let mut v2 = vector::Vector::new(2);
@@ -55,7 +55,7 @@ mod tests {
     }
     #[test]
     fn it_tests_vector_to_vector_subtraction() {
-        use crate::vector::vector;
+        use crate::vector;
         let mut v1 = vector::Vector::new(2);
         v1.set(vec![2.0, 3.0]);
         let mut v2 = vector::Vector::new(2);
@@ -65,7 +65,7 @@ mod tests {
     }
     #[test]
     fn it_tests_vector_to_vector_multimplication() {
-        use crate::vector::vector;
+        use crate::vector;
         let mut v1 = vector::Vector::new(2);
         v1.set(vec![2.0, 3.0]);
         let mut v2 = vector::Vector::new(2);
@@ -75,7 +75,7 @@ mod tests {
     }
     #[test]
     fn it_tests_vector_to_vector_division() {
-        use crate::vector::vector;
+        use crate::vector;
         let mut v1 = vector::Vector::new(2);
         v1.set(vec![2.0, 3.0]);
         let mut v2 = vector::Vector::new(2);
@@ -85,7 +85,7 @@ mod tests {
     }
     #[test]
     fn it_tests_dot_product_of_two_vectors() {
-        use crate::vector::vector;
+        use crate::vector;
         let mut v1 = vector::Vector::new(3);
         v1.set(vec![1.0, 2.0, 3.0]);
         let mut v2 = vector::Vector::new(3);
@@ -94,7 +94,7 @@ mod tests {
     }
     #[test]
     fn it_tests_matrix_construction() {
-        use crate::matrix::matrix;
+        use crate::matrix;
         let shape = matrix::Matrix::new(2, 2).shape();
         let data = matrix::Matrix::new(2, 2).as_vec();
         let vec = vec![vec![0.0; 2]; 2];
@@ -102,7 +102,7 @@ mod tests {
     }
     #[test]
     fn it_tests_matrix_ones_construction() {
-        use crate::matrix::matrix;
+        use crate::matrix;
         let shape = matrix::Matrix::ones(2, 2).shape();
         let data = matrix::Matrix::ones(2, 2).as_vec();
         let vec = vec![vec![1.; 2]; 2];
@@ -110,14 +110,14 @@ mod tests {
     }
     #[test]
     fn it_tests_random_matrix_construction() {
-        use crate::matrix::matrix;
+        use crate::matrix;
         let m = matrix::Matrix::random((2, 2), (-1., 0.));
         assert_ne!(m.as_vec()[0][0], m.as_vec()[0][1]);
         assert_ne!(m.as_vec()[1][0], m.as_vec()[1][1]);
     }
     #[test]
     fn it_tests_matrix_set() {
-        use crate::matrix::matrix;
+        use crate::matrix;
         let mut m = matrix::Matrix::random((2, 2), (-1., 0.));
         m.set(vec![vec![0.; 2]; 2]);
         let vec = vec![vec![0.; 2]; 2];
@@ -125,7 +125,7 @@ mod tests {
     }
     #[test]
     fn it_tests_matrix_multiplication() {
-        use crate::matrix::matrix;
+        use crate::matrix;
         let mut m = matrix::Matrix::new(2, 3);
         m.set(vec![vec![1., 2., 3.], vec![4., 5., 6.]]);
         let mut m2 = matrix::Matrix::new(3, 2);
@@ -135,7 +135,7 @@ mod tests {
     }
     #[test]
     fn it_tests_matrix_transposition() {
-        use crate::matrix::matrix;
+        use crate::matrix;
         let mut m = matrix::Matrix::new(2, 3);
         m.set(vec![vec![1., 3., 5.], vec![2., 4., 6.]]);
         assert_eq!(m.T().as_vec(), vec![vec![1., 2.], vec![3., 4.], vec![5., 6.]]);
